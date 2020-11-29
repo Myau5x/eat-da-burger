d2eba9fe05ee19fb399f68c22ba8f234875782f1
Olga Illarionova <ola.illari@gmail.com>
2020-11-29 12:22:32 -0800

    create db

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Fishburger');
+
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Chikenburger');
