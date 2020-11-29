1d91070c01d4eb4fddd22ee2e51fbb3002c5a8e9
Olga Illarionova <ola.illari@gmail.com>
2020-11-29 12:09:33 -0800

    create connection

---
+++ connection.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+var connection;
+connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "employeeDB"
+  });
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
+  });
+
+module.exports= connection;
