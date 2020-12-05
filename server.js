4f8fbd6b9b948fd5fdea1a50c289f9cba03dff71
Olga Illarionova <ola.illari@gmail.com>
2020-12-04 20:28:27 -0800

    add some comments

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burger_controller");
 
-app.use(routes);
+app.use(routes); /// to actualy listen that routes
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
