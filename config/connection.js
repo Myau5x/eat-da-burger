df226004def8c599a1f2f165f8b681ba5d015961
Olga Illarionova <ola.illari@gmail.com>
2020-12-04 21:01:45 -0800

    add jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
 connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: "burger_db"
-  });
+  });}
 
 connection.connect(function(err) {
     if (err) {
