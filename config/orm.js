d6520f3de347310e795ff468e5bbcd24844ce954
Olga Illarionova <ola.illari@gmail.com>
2020-12-04 16:38:28 -0800

    add callbacks to orm

--- config/orm.js
+++ config/orm.js
@@ -1,35 +1,35 @@
 const connection = require("./connection");
 
 const ORM = {
-    selectAll : function(table) {
+    selectAll : function(table, cb) {
       let queryString = "SELECT * FROM ??;";
       connection.query(queryString,[table], function(err, data) {
         if (err) {
-            console.log(err)
+            throw err;
           
         }
     
-        console.log(data)
+        cb(data);
       }) },
-    insertOne : function(table, col_name, value) {
+    insertOne : function(table, col_name, value, cb) {
       let queryString = "INSERT INTO ?? (??) VALUES (?);"
       connection.query(queryString,[table,col_name,value], function(err, data) {
         if (err) {
-            console.log(err)
+            throw err;
           
         }
     
-        console.log(data)
+        cb(data)
       }) },
-    updateOne : function(table, update_col, value, id) {
+    updateOne : function(table, update_col, value, id,cb) {
       let queryString = "UPDATE ?? SET ?? = ? WHERE id = ?"
       connection.query(queryString,[table, update_col,value,id], function(err, data) {
         if (err) {
-            console.log(err)
+            throw err;
           
         }
     
-        console.log(data)
+        cb(data)
       }) },
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //const connection = require("./config/connection");
 const ORM = require("./config/orm");
+const express = require("express");
 
 //ORM.insertOne("burgers","burger_name","MozarellaBurger");
-ORM.updateOne('burgers','devoured',1,3);
-ORM.selectAll("burgers");
+//ORM.updateOne('burgers','devoured',1,3);
+ORM.selectAll("burgers", console.log);
